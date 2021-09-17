7b1c595a1e760cc989e122655408a972e39accd7
hutinoatari <63452799+hutinoatari@users.noreply.github.com>
2021-09-18 01:37:40 +0900

    スクロールしても表の下部が隠れないようにした

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React, { FC } from 'react';
-import { StyleSheet, SafeAreaView, StatusBar, View, Text, Button, TextInput } from 'react-native';
+import { StyleSheet, SafeAreaView, StatusBar, View, ScrollView, Text, Button, TextInput } from 'react-native';
 import { Table, Row, Cell } from "./components/Table";
 
 const App: FC<{}> = () => {
@@ -8,7 +8,6 @@ const App: FC<{}> = () => {
     <SafeAreaView style={{
       flex: 1,
       paddingTop: StatusBar.currentHeight,
-      width: "100%",
     }}>
       <ExpoStatusBar />
 
@@ -49,36 +48,45 @@ const App: FC<{}> = () => {
           flexDirection: "row",
         }}>
           <Text>期限: </Text>
-          <TextInput style={{
-            backgroundColor: "white",
-            borderWidth: 1,
-            padding: 0,
-            flex: 1,
-          }} />
+          <Text>2021/9/18</Text>
+          <Button title="日付選択" onPress={() => {}} />
         </View>
         <Button title="追加する" onPress={() => {}} />
       </View>
 
       <View style={{
+        flex: 1,
         backgroundColor: "#ffffaa",
       }}>
         <Text>課題リスト</Text>
-        <Table>
-          <Row>
-            <Cell><Text>名前</Text></Cell>
-            <Cell><Text>期限</Text></Cell>
-            <Cell><Text>状態</Text></Cell>
-            <Cell><Text>完了</Text></Cell>
-            <Cell><Text>削除</Text></Cell>
-          </Row>
-          <Row>
-            <Cell><Text>課題A</Text></Cell>
-            <Cell><Text>2021/10/12</Text></Cell>
-            <Cell><Text>x</Text></Cell>
-            <Cell><Button title="完了" onPress={() => {}} /></Cell>
-            <Cell><Button title="削除" onPress={() => {}} /></Cell>
-          </Row>
-        </Table>
+        <Button title="更新する" onPress={() => {}} />
+        <ScrollView>
+          <Table>
+            <Row>
+              <Cell><Text>名前</Text></Cell>
+              <Cell><Text>期限</Text></Cell>
+              <Cell><Text>状態</Text></Cell>
+              <Cell><Text>完了</Text></Cell>
+              <Cell><Text>削除</Text></Cell>
+            </Row>
+          </Table>
+          <Table>
+            <Row>
+              <Cell><Text>課題A</Text></Cell>
+              <Cell><Text>2021/9/18</Text></Cell>
+              <Cell><Text>x</Text></Cell>
+              <Cell><Button title="完了" onPress={() => {}} /></Cell>
+              <Cell><Button title="削除" onPress={() => {}} /></Cell>
+            </Row>
+            <Row>
+              <Cell><Text>課題B</Text></Cell>
+              <Cell><Text>2021/10/12</Text></Cell>
+              <Cell><Text>o</Text></Cell>
+              <Cell><Button title="完了" onPress={() => {}} /></Cell>
+              <Cell><Button title="削除" onPress={() => {}} /></Cell>
+            </Row>
+          </Table>
+        </ScrollView>
       </View>
 
       <View style={{
