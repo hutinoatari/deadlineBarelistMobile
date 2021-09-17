b290615fb1835570c4cd649f25c8e1bf3a88e1af
hutinoatari <63452799+hutinoatari@users.noreply.github.com>
2021-09-17 22:06:23 +0900

    アロー関数に書き換えた

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { FC } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App: FC<{}> = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -19,3 +19,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
